9a31935f189c05d8305424cbb380c56ddad08d5c
Estevão <estevao.uyra.pv@gmail.com>
2023-12-30 10:38:30 +0100

    add cdn url

--- sunbraid/head.py
+++ sunbraid/head.py
@@ -1,18 +1,13 @@
-def get_all_functions():
-    with open('sunbraid/cell/line.js') as f:
-        line = '<script>' + f.read() + '</script>'
-
-    return line
 
 def get_imports():
     return """
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://d3js.org/d3.v6.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/gh/estevaouyra/sunbraid@v0.0.1-alpha/sunbraid/cell/line.js"></script>
 """
+
 def make_head():
-    all_functions = get_all_functions()
     all_imports = get_imports()
-    return '<head>' + all_functions + all_imports + '</head>'
+    return '<head>' + all_imports + '</head>'
 
 
