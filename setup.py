46d71cc9eedffeb75d3157fa1f8af972391f25ed
Estevão <estevao.uyra.pv@gmail.com>
2024-01-03 12:22:39 +0100

    add package on setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         # author_email='estevao.uyra.pv@gmail.com',
         # use_scm_version=True,
         # setup_requires=['setuptools_scm'],
-        # packages=find_packages(include=['sunbraid']),   
+        packages=find_packages(include=['sunbraid']),   
         # package_data={
         # "sunbraid": ["static.json"],
     # }
