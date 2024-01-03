2b4be3a0ec45e1ed4f1929143fb25fca0cc8b374
Estevão <estevao.uyra.pv@gmail.com>
2024-01-03 10:57:40 +0100

    add name to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
 from setuptools import find_packages
+from sunbraid._version import __version__
 
 if __name__ == "__main__":
     setuptools.setup(
+        name='sunbraid',  # Replace with your package's name
+        author='Estevão Uyrá',
+        author_email='estevao.uyra.pv@gmail.com',
+        version=__version__,
         packages=find_packages(include=['sunbraid']),   
         package_data={
         "sunbraid": ["static.json"],
